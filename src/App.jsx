c2102dd43fc7e14223127429633e60e551dff4e2
kibirige <kibirigejohn005@gmail.com>
2023-02-09 15:16:51 +0300

    create admin, parent, teacher, student dashboards and their routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import "./App.css";
 import AdminLogin from "./components/login&signup/Login";
 import { Route, Routes } from "react-router-dom";
+import AdminDashboard from "./components/admin/AdminDashboard";
+import TeacherDashboard from "./components/teacher/TeacherDashboard";
+import StudentDashboard from "./components/student/StudentDashboard";
+import ParentDashboard from "./components/parent/ParentDashboard";
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<AdminLogin />} />
+        <Route path="/admin" element={<AdminDashboard />} />
+        <Route path="/teacher" element={<TeacherDashboard />} />
+        <Route path="/student" element={<StudentDashboard />} />
+        <Route path="/parent" element={<ParentDashboard />} />
       </Routes>
     </div>
   );
